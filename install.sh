cbcd10ddc9f05b24983d217cd631925e6f467488
stdereka <st.dereka@gmail.com>
2018-11-30 19:26:05 +0300

    Added installation script and removed empty files

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+script_dir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd)
+cd $script_dir
+touch domains.txt index.txt mail.txt e-mails.txt log.txt users.txt
+echo "alias start_parser='python3 $script_dir/parser.py >> $script_dir/log.txt 2>&1 &'" >> ~/.bashrc
+source ~/.bashrc

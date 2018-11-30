5acc3fcc578ff022a36e5fa4f607e2c4df0cdd5a
stdereka <st.dereka@gmail.com>
2018-11-30 19:34:58 +0300

    Some fixes

--- install.sh
+++ install.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 script_dir=$(cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd)
 cd $script_dir
-touch domains.txt index.txt mail.txt e-mails.txt log.txt users.txt
+touch domains.txt index.txt mail.txt e-mails.txt log.txt users.txt keywords.txt
 echo "alias start_parser='python3 $script_dir/parser.py >> $script_dir/log.txt 2>&1 &'" >> ~/.bashrc
-source ~/.bashrc
